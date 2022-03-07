bdca69a61633b0eed46c1aad776397f2d5549986
Subhojit <ascinate7@gmail.com>
2022-03-07 20:55:08 +0530

    UI/UX

---
+++ IT-Company/contact.php
@@ -0,0 +1,50 @@
+<?php
+
+
+    $to = "samrat.subratadas@gmail.com";
+    $subject = $_POST['Contact Us - IT Firm'];
+
+    $message = "
+    <html>
+    <body>
+    <table>
+
+    <tr>
+    <td>Name</td>
+    <td>".$_POST['name']."</td>
+    </tr>
+    <tr>
+    <td>Subject</td>
+    <td>".$_POST['subject']."</td>
+    </tr>
+    <tr>
+    <td>Phone</td>
+    <td>".$_POST['phone']."</td>
+    </tr>
+    <tr>
+    <td>Email</td>
+    <td>".$_POST['email']."</td>
+    </tr>  
+    <td>Message</td>
+    <td>".$_POST['message']."</td>
+    </tr>
+
+
+
+    </table>
+    </body>
+    </html>
+    ";
+
+    // Always set content-type when sending HTML email
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+    // More headers
+    $headers .= 'From: <info@marhansolutions.com>' . "\r\n";
+
+    mail($to,$subject,$message,$headers);
+
+    header('location: thankyou.html');
+
+?>
